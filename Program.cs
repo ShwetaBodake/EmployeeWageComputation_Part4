ec42a061c9ac69bcc02a801627f6a8a2a35c7335
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-25 23:48:08 +0530

    Solution:Daily Employee wage program

--- Program.cs
+++ Program.cs
@@ -7,24 +7,33 @@ namespace EmployeeWageComputation_Part4
         public static void Main(string[] args)
         {
             Console.WriteLine("Wel_Come to Employee Wage Computation Problem");
-            AttendanceCheck x = new AttendanceCheck();
-            x.EmpAttendace();
+            DailyEmpWage_UC2 dailyWage = new DailyEmpWage_UC2();
+            dailyWage.DailyWage();
         }
 
-        public class AttendanceCheck
+
+        public class DailyEmpWage_UC2
         {
-            public void EmpAttendace()
+            public void DailyWage()
             {
+                int Wage_per_Hour = 20;
+                int Full_Day_Hour = 8;
                 Random random = new Random();
                 int isPresent = random.Next(0, 2);
 
                 //Console.WriteLine(isPresent);
                 if (isPresent == 1)
-                { Console.WriteLine("Employee is PRESENT"); }
+                {
+                    Console.WriteLine("Employee is PRESENT");
+                    int DailyWge = Wage_per_Hour * Full_Day_Hour;
+                    Console.WriteLine("Daily wage of employee is " + DailyWge);
+
+                }
                 else
                 {
                     Console.WriteLine("Employee is Absent");
                 }
+
 
             }
         }
