82874c7b37527d978e7eb55372d2221e37f33a28
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-25 23:39:52 +0530

    Solution :Employee check attendane

--- Program.cs
+++ Program.cs
@@ -7,6 +7,27 @@ namespace EmployeeWageComputation_Part4
         public static void Main(string[] args)
         {
             Console.WriteLine("Wel_Come to Employee Wage Computation Problem");
+            AttendanceCheck x = new AttendanceCheck();
+            x.EmpAttendace();
         }
+
+        public class AttendanceCheck
+        {
+            public void EmpAttendace()
+            {
+                Random random = new Random();
+                int isPresent = random.Next(0, 2);
+
+                //Console.WriteLine(isPresent);
+                if (isPresent == 1)
+                { Console.WriteLine("Employee is PRESENT"); }
+                else
+                {
+                    Console.WriteLine("Employee is Absent");
+                }
+
+            }
+        }
+
     }
 }
