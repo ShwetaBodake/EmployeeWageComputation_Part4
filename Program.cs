91d6b1bb936554d507da7fc016c9e1cd23db2d47
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-25 19:04:38 +0530

    WelCome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace EmployeeWageComputation_Part4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wel_Come to Employee Wage Computation Problem");
+        }
+    }
+}
